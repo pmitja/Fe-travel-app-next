1978810d171bd083e62b018d95b3fea75f9323fc
Mitja Pak <pmitja1@gmail.com>
2023-05-18 20:22:05 +0200

    Added the range picker component.

---
+++ app/components/inputs/RangeDatePicker.tsx
@@ -0,0 +1,30 @@
+import DatePicker from "antd/lib/date-picker";
+
+const { RangePicker } = DatePicker;
+
+const RangeDatePicker = () => {
+  return (
+    <RangePicker
+      size={"large"}
+      format={"DD MMM YYYY"}
+      className="
+    w-full 
+    rounded-md 
+    border-0 
+    py-1.5 
+    text-gray-900 
+    shadow-sm 
+    ring-1 
+    ring-inset 
+    ring-gray-300 
+    placeholder:text-gray-400 
+    focus:ring-2 
+    focus:ring-inset 
+    focus:ring-sky-600 
+    sm:text-sm 
+    sm:leading-6"
+    />
+  );
+};
+
+export default RangeDatePicker;
